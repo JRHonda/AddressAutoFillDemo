ac0cc6b510c8d60a4eaba95b0c69b13d2a25f36e
Justin Honda <justin@zerocool.com>
2023-02-22 12:27:38 -0500

    Initial Commit

---
+++ AddressAutoFillDemo/AddressAutoFillDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressAutoFillDemoApp.swift
+//  AddressAutoFillDemo
+//
+//  Created by Justin Honda on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AddressAutoFillDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
